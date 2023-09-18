07044281bd5f929e4e466a0f78e03a7637b85113
sidharth <sidharthkumar@gmail.com>
2023-09-18 14:57:34 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Assignment3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace As3
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+            Assignment3.Program program = new Assignment3.Program();
+            Console.WriteLine(program.name);
+        }
+    }
+}
